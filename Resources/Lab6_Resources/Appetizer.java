e445d42bf94f492f03ee6aefa903f4b133d2cc13
monkeywithbanana <>
2020-06-06 20:45:35 -0700

    Revert "adding lab6 resources"
    This reverts commit dfb1936bfcd2685abfa06a436cfcf18184ad5c9b.

--- Resources/Lab6_Resources/Appetizer.java
+++
@@ -1,8 +0,0 @@
-package com.pocurestaurant.menu;
-
-public enum Appetizer {
-    CALAMARI,
-    GYOZA,
-    SPINACH_DIP,
-    NACHOS
-}
--- Resources/Lab6_Resources/Dessert.java
+++
@@ -1,9 +0,0 @@
-package com.pocurestaurant.menu;
-
-public enum Dessert {
-    MANGO_PUDDING,
-    GREEN_TEA_ICE_CREAM,
-    CHOCOLATE_MOUSSE,
-    APPLE_PIE,
-    ITALIAN_DONUTS
-}
